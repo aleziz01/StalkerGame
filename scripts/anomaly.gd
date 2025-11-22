dccb6859cb6c6bad9bac52086e8e2d7c97a52b40
aleziz01 <alex.artene@gmail.com>
2025-11-22 20:55:28 +0100

    stopping for now

--- scripts/anomaly.gd
+++ scripts/anomaly.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("entity") && !body.dead:
+		spin(body)
+
+func spin(body):
+	var radius=121
+	while true:
+		await get_tree().create_timer(get_process_delta_time())
+		
