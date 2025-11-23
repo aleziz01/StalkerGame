e8e09cac8c7205181010f0a417e274337197dad9
aleziz01 <alex.artene@gmail.com>
2025-11-23 13:15:52 +0100

    anomaly done, need sfx

--- scripts/anomaly.gd
+++ scripts/anomaly.gd
@@ -5,8 +5,30 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("entity") && !body.dead:
 		spin(body)
 
+@onready var attack_particles: GPUParticles2D = $AttackParticles
+
 func spin(body):
 	var radius=121
+	var center=Vector2(0,0)
+	var accel=0
+	var rotationdeg=body.rotation_degrees
+	attack_particles.emitting=true
+	if body.name=="Character":
+		body.get_child(4).enabled=false
+		$Camera2D.enabled=true
 	while true:
-		await get_tree().create_timer(get_process_delta_time())
-		
+		var delta=get_process_delta_time()
+		body.global_position=global_position+Vector2(radius*cos(rotationdeg),radius*sin(rotationdeg))
+		rotationdeg+=delta*(20+accel)
+		radius-=delta*20
+		accel+=delta*5
+		radius=clamp(radius,0,121)
+		if radius<5:
+			body.rotation_degrees=rotationdeg
+			explode(body)
+			break
+		await get_tree().create_timer(delta).timeout
+
+func explode(body):
+	body.dead=true
+	
--- scripts/character.gd
+++ scripts/character.gd
@@ -2,28 +2,31 @@ extends CharacterBody2D
 
 
 const SPEED = 10000.0
-
+var dead=false
 func _physics_process(delta: float) -> void:
-	look_at(get_global_mouse_position())
-	var directionx := Input.get_axis("left", "right")
-	var directiony := Input.get_axis("up","down")
-	if directionx:
-		velocity.x+=directionx*SPEED*delta
-		velocity.x=clamp(velocity.x,-300,300)
+	if !dead:
+		look_at(get_global_mouse_position())
+		var directionx := Input.get_axis("left", "right")
+		var directiony := Input.get_axis("up","down")
+		if directionx:
+			velocity.x+=directionx*SPEED*delta
+			velocity.x=clamp(velocity.x,-300,300)
+		else:
+			velocity.x=0
+		if directiony:
+			velocity.y+=directiony*SPEED*delta
+			velocity.y=clamp(velocity.y,-300,300)
+		else:
+			velocity.y=0
 	else:
-		velocity.x=0
-	if directiony:
-		velocity.y+=directiony*SPEED*delta
-		velocity.y=clamp(velocity.y,-300,300)
-	else:
-		velocity.y=0
+		velocity=Vector2.ZERO
 	move_and_slide()
 
 @onready var shootTimer: Timer = $shootTimer
 var shootable=true
 var weaponName="makarov"
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("shoot") && shootable:
+	if Input.is_action_pressed("shoot") && shootable && !dead:
 		shootable=false
 		shoot(weaponName)
 
