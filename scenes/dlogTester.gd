4cb3ac4fb61bb9041af2794a172d63062a3b898d
aleziz01 <alex.artene@gmail.com>
2025-11-23 14:21:41 +0100

    basically no changes

--- scenes/dlogTester.gd
+++ scenes/dlogTester.gd
@@ -1,43 +1,43 @@
 extends Sprite2D
 
-const TESTER = preload("uid://dyx24at1yuqet")
-var dlogActive = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	DialogueManager.dialogue_started.connect(dlogStarted)
-	DialogueManager.dialogue_ended.connect(dlogEnded)
+#TO UNCOMMENT THIS FILE DO CTRL+F AND TYPE #, THEN CLICK ON THE CODE AND THEN USE THE ARROW AND BACKSPACE TO CLEAR ALL #s QUICKLY
 
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("dlog") and Global.entered == true and dlogActive == false:
-		DialogueManager.show_dialogue_balloon(TESTER,"start")
-	if Input.is_action_just_pressed("dlog") and Global.entered == true and dlogActive == true:
-		DialogueManager.show_dialogue_balloon(TESTER,"start").hide()
-
-	pass
-
-
-func _on_dlog_g_area_entered(area: Area2D) -> void:
-	Global.entered = true
-	pass # Replace with function body.
-
-
-func _on_dlog_g_area_exited(area: Area2D) -> void:
-	Global.entered = false
-	pass # Replace with function body.
-
-
-
-func dlogStarted(TESTER):
-	print('dasdasd')
-	dlogActive = true
-	pass # Replace with function body.
-	
-func dlogEnded(TESTER):
-	await get_tree().create_timzer(0.2).timeout
-	print('fdasfadf')
-	dlogActive = false
-	pass # Replace with function body.
+#
+#const TESTER = preload("uid://dyx24at1yuqet")
+#var dlogActive = false
+ ##Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#DialogueManager.dialogue_started.connect(dlogStarted)
+	#DialogueManager.dialogue_ended.connect(dlogEnded)
+#
+#pass  #Replace with function body.
+#
+#
+ ##Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("dlog") and Global.entered == true and dlogActive == false:
+		#DialogueManager.show_dialogue_balloon(TESTER,"start")
+	#if Input.is_action_just_pressed("dlog") and Global.entered == true and dlogActive == true:
+			#DialogueManager.show_dialogue_balloon(TESTER,"start").hide()
+	#pass
+#
+#
+#func _on_dlog_g_area_entered(area: Area2D) -> void:
+	#Global.entered = true
+#
+#func _on_dlog_g_area_exited(area: Area2D) -> void:
+	#Global.entered = false
+	##pass  Replace with function body.
+#
+#
+#
+#func dlogStarted(TESTER):
+	#print('dasdasd')
+	#dlogActive = true
+	##pass  Replace with function body.
+	#
+#func dlogEnded(TESTER):
+	#await get_tree().create_timzer(0.2).timeout
+	#print('fdasfadf')
+	#dlogActive = false
+	##pass  Replace with function body.
